b49f72bad4a83b063b1acf85320e25c9b8fb52fd
Vladimir <eurasian@list.ru>
2023-12-12 17:55:35 +0300

    Написана процедура заполнение массива ArrayFirst

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+string [] sourceArray = new string [m];
+
+
+void ArrayFirst(string [] sourceArray)
+{
+  for (int i = 0; i < sourceArray.Length; i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     sourceArray[i] = Console.ReadLine();
+  }
+}
+
+
