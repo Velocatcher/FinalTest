4c5dafb1d8e728385be18a47a54a119222e90fdc
Vladimir <eurasian@list.ru>
2023-12-12 19:23:50 +0300

    Продолжаем редактировать файл README.MD

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ void ArrayFirst(string [] sourceArray)
 {
   for (int i = 0; i < sourceArray.Length; i++)
   {
-     Console.WriteLine($"Введите {i+1} элемент массива");
-     sourceArray[i] = Console.ReadLine();
+    Console.WriteLine($"Введите {i+1} элемент массива");
+    sourceArray[i] = Console.ReadLine();
   }
 }
 
